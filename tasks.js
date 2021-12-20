2190ec5a1a3ab88249ef9ddd815a5ef0198b442e
zaynab-abdelnabi <zaynab.abdelnabi@gmail.com>
2021-12-20 22:21:25 +0200

    help command

--- tasks.js
+++ tasks.js
@@ -40,6 +40,9 @@ function onDataReceived(text) {
   else if(text === 'hello\n'){
     hello();
   }
+  else if(text === 'help\n'){
+    help();
+  }
   else{
     unknownCommand(text);
   }
@@ -76,6 +79,10 @@ function hello(){
 function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
+}
+
+ function help(){
+  console.log("'hello' to say hello \n'quit' or 'exit' to quit \n'help' to list the commands")
 }
 
 // The following line starts the application
